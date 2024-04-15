94c69ee0fd2c045ce1b15e58e7565b520b31f52f
0mariapaula <0mariapaulaferreira@gmail.com>
2024-04-15 16:17:47 -0300

    Voltando

--- src/pontoDeDoacao/pontoDeDoacao.controller.ts
+++ src/pontoDeDoacao/pontoDeDoacao.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Post, Patch, Param, Body, Delete } from '@nestjs/common';
 import { PontoDeDoacaoService } from './pontoDeDoacao.service';
-import { CreatePontoDeDoacaoDto, UpdatePontoDeDoacaoDto } from '../dtos/pontoDeDoacao.dto';
 
 @Controller('pontosDeDoacao')
 export class PontoDeDoacaoController {
@@ -14,16 +13,6 @@ export class PontoDeDoacaoController {
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.pontoDeDoacaoService.findOne(id);
-  }
-
-  @Post()
-  create(@Body() createPontoDeDoacaoDto: CreatePontoDeDoacaoDto) {
-    return this.pontoDeDoacaoService.create(createPontoDeDoacaoDto);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: number, @Body() updatePontoDeDoacaoDto: UpdatePontoDeDoacaoDto) {
-    return this.pontoDeDoacaoService.update(id, updatePontoDeDoacaoDto);
   }
 
   @Delete(':id')
--- src/pontoDeDoacao/pontoDeDoacao.service.ts
+++ src/pontoDeDoacao/pontoDeDoacao.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { PontoDeDoacao } from '../entidades/pontosdedoacao';
-import { CreatePontoDeDoacaoDto, UpdatePontoDeDoacaoDto } from '../dtos/pontoDeDoacao.dto';  // Assumindo que você criou DTOs
+
 
 @Injectable()
 export class PontoDeDoacaoService {
@@ -16,16 +16,6 @@ export class PontoDeDoacaoService {
   }
 
   findOne(id: number): Promise<PontoDeDoacao> {
-    return this.pontoDeDoacaoRepository.findOneBy({ id });
-  }
-
-  create(createPontoDeDoacaoDto: CreatePontoDeDoacaoDto): Promise<PontoDeDoacao> {
-    const novoPonto = this.pontoDeDoacaoRepository.create(createPontoDeDoacaoDto);
-    return this.pontoDeDoacaoRepository.save(novoPonto);
-  }
-
-  async update(id: number, updatePontoDeDoacaoDto: UpdatePontoDeDoacaoDto): Promise<PontoDeDoacao> {
-    await this.pontoDeDoacaoRepository.update(id, updatePontoDeDoacaoDto);
     return this.pontoDeDoacaoRepository.findOneBy({ id });
   }
 
